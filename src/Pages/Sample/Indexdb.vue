131cbc9f033f87520903b2c101f1dd17d98bda21
mysaltern <mysaltern@gmail.com>
2020-05-17 22:46:04 +0430

    second

--- src/Pages/Sample/Indexdb.vue
+++ src/Pages/Sample/Indexdb.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
- 
-<i class="fas fa-plug fa-3x"></i>
-
-<i class="fas fa-unlink fa-3x"></i>
-
+  <div>
+    <div v-if="Math.random() > 0.5">
+      <i class="fas fa-plug fa-3x"></i>
     </div>
+    <div v-else>
+      <i class="fas fa-wifi fa-3x" style="color:green"></i>
+    </div>
+  </div>
 </template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
